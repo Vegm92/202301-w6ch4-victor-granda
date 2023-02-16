c7528273b4ca44cffaedb4a760ce8515130d4adf
Victor Granda <victorgranda1992@gmail.com>
2023-02-16 18:38:44 +0100

    Add external dependencies created env. and started the server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-a;
+import "./loadEnvironments.js";
+import express from "express";
+import createDebug from "debug";
+import chalk from "chalk";
+import morgan from "morgan";
+
+const debug = createDebug("challenge-api");
+const port = process.argv[3] ?? process.env.PORT ?? 5000;
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(
+  (req, res, next) => {
+    debug("I'm Alive!");
+    next();
+  },
+  (req, res) => {
+    res.status(200).json();
+  }
+);
+
+app.listen(port, () => {
+  debug(chalk.cyan.bold(`Server started at http://localhost:${port}`));
+});
